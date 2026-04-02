2abd8050b6c633c67ccdf1051c3793154ad87c1f
Hoaqf <szymon.thiede1@outlook.com>
2026-04-02 21:13:38 +0200

    Zad 3.5: Endpointy CRUD dla listy produktow

--- 2/src/main/scala/controllers/ProductController.scala
+++ 2/src/main/scala/controllers/ProductController.scala
@@ -3,15 +3,51 @@ package controllers
 import org.scalatra._
 import org.scalatra.json._
 import org.json4s.{DefaultFormats, Formats}
+import models.Product
 
 class ProductController extends ScalatraServlet with JacksonJsonSupport {
   protected implicit lazy val jsonFormats: Formats = DefaultFormats
+
+  var products = List(
+    Product(1, "Laptop", 3500.0),
+    Product(2, "Myszka", 150.0)
+  )
 
   before() {
     contentType = formats("json")
   }
 
   get("/") {
-    "Kontroler Produktow dziala!"
+    products
+  }
+
+  get("/:id") {
+    val id = params("id").toInt
+    products.find(_.id == id) match {
+      case Some(product) => product
+      case None => halt(404, "Product not found")
+    }
+  }
+
+  post("/") {
+    val newProduct = parsedBody.extract[Product]
+    products = products :+ newProduct
+    newProduct
+  }
+
+  put("/:id") {
+    val id = params("id").toInt
+    val updatedProduct = parsedBody.extract[Product]
+    products = products.map {
+      case p if p.id == id => updatedProduct.copy(id = id)
+      case p => p
+    }
+    updatedProduct
+  }
+
+  delete("/:id") {
+    val id = params("id").toInt
+    products = products.filterNot(_.id == id)
+    Ok(s"Deleted product $id")
   }
 }
